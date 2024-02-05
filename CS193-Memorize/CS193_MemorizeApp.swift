7aa3b9941863fb96c4e55a285d46219b0e661969
hongjie.wu <hongjie.wu@shopee.com>
2024-02-05 12:06:52 +0800

    Initial Commit

---
+++ CS193-Memorize/CS193_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193_MemorizeApp.swift
+//  CS193-Memorize
+//
+//  Created by HongjieWu on 2024/2/5.
+//
+
+import SwiftUI
+
+@main
+struct CS193_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193-Memorize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CS193-Memorize
+//
+//  Created by HongjieWu on 2024/2/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
